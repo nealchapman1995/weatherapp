159b26f897a1fbab36fc581eb4b3f5b58c204bc8
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2024-06-21 12:17:10 -0600

    added api key and loaded json

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+const apiKey = process.env.REACT_APP_API_KEY;
+
+
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=${apiKey}`)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []); // Empty dependency array ensures this runs only once after initial render
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World!</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
 
 export default App;
+
