78be82ca6d4ce3546ae98797f332e0dcd10a38ee
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2024-06-25 15:06:09 -0600

    added temp min and max

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     console.log("Fetching data..."); // Debug log
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=${apiKey}&units=imperial`)
       .then(response => response.json())
       .then(data => {
         console.log("Data fetched:", data); // Debug log
@@ -25,16 +25,22 @@ function App() {
   const groupedData = data.list.reduce((acc, item) => {
     const date = new Date(item.dt * 1000).toLocaleDateString();
     if (!acc[date]) {
-      acc[date] = { count:0, totalPop: 0};
+      acc[date] = { count:0, totalPop: 0, temp_min: item.main.temp_min, temp_max: item.main.temp_max, icon: item.weather[0].icon};
     }
     acc[date].count += 1;
     acc[date].totalPop += item.pop;
+    if (acc[date].temp_min > item.main.temp_min){
+      acc[date].temp_min = item.main.temp_min
+    }
+    if (acc[date].temp_max < item.main.temp_max){
+      acc[date].temp_max = item.main.temp_max
+    }
     return acc
   }, {})
 
  const dates = Object.keys(groupedData).map(date => {
   const averagePop = (groupedData[date].totalPop / groupedData[date].count) * 100;
-  return { date, averagePop: averagePop.toFixed(1) };
+  return { date, averagePop: averagePop.toFixed(1), temp_min: groupedData[date].temp_min, temp_max: groupedData[date].temp_max, icon:groupedData[date].icon};
  })
 
   
@@ -46,6 +52,8 @@ function App() {
         <div key={index}>
         <h5>{item.date}</h5>
         <p>Rain Probability: {item.averagePop}%</p>
+        <p>highest temp! {item.temp_max} Degrees F</p>
+        <p>Lowest temp! {item.temp_min} Degrees F</p>
         </div>
       ))}
       <pre>{JSON.stringify(data.list, null, 2)}</pre>
