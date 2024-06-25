45b61090a83e41ddb24b68ece413f618f4e3b666
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2024-06-25 13:17:06 -0600

    Added the ability to show dates and also probability of percepitation

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -2,29 +2,57 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 const apiKey = process.env.REACT_APP_API_KEY;
 
-
-
 function App() {
   const [data, setData] = useState(null);
 
   useEffect(() => {
+    console.log("Fetching data..."); // Debug log
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=${apiKey}`)
       .then(response => response.json())
-      .then(data => setData(data))
+      .then(data => {
+        console.log("Data fetched:", data); // Debug log
+        setData(data);
+      })
       .catch(error => console.error('Error fetching data:', error));
   }, []); // Empty dependency array ensures this runs only once after initial render
 
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  const city = data.city.name;
+
+  const groupedData = data.list.reduce((acc, item) => {
+    const date = new Date(item.dt * 1000).toLocaleDateString();
+    if (!acc[date]) {
+      acc[date] = { count:0, totalPop: 0};
+    }
+    acc[date].count += 1;
+    acc[date].totalPop += item.pop;
+    return acc
+  }, {})
+
+ const dates = Object.keys(groupedData).map(date => {
+  const averagePop = (groupedData[date].totalPop / groupedData[date].count) * 100;
+  return { date, averagePop: averagePop.toFixed(1) };
+ })
+
+  
+
   return (
     <div className="App">
-      <h1>Hello World!</h1>
-      {data ? (
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <h1>{city}</h1>
+      {dates.map((item, index) => (
+        <div key={index}>
+        <h5>{item.date}</h5>
+        <p>Rain Probability: {item.averagePop}%</p>
+        </div>
+      ))}
+      <pre>{JSON.stringify(data.list, null, 2)}</pre>
     </div>
   );
 }
 
 export default App;
+
 
