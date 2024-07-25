36770d3156f4b06d8750923df2dbf19eb9895572
nealchapman1995 <61425224+nealchapman1995@users.noreply.github.com>
2024-07-25 11:56:56 -0600

    Updated it so that detailed view will open in the middle and above the day view

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import DetailedView from './detailedView';
 const apiKey = process.env.REACT_APP_API_KEY;
 
-
 function App() {
   const [data, setData] = useState(null);
   const [selectedCity, setSelectedCity] = useState('Denver');
@@ -21,7 +20,7 @@ function App() {
         setData(data);
       })
       .catch(error => console.error('Error fetching data:', error));
-  }; // Empty dependency array ensures this runs only once after initial render
+  };
 
   const handleCityChange = (e) => {
     setSelectedCity(e.target.value);
@@ -31,10 +30,9 @@ function App() {
   const handleDayClick = (dayData) => {
     const filteredData = data.list.filter(item => {
       const date = new Date(item.dt * 1000).toLocaleDateString();
-      return date == dayData.date;
+      return date === dayData.date;
     });
-    setSelectedDay({...dayData, details: filteredData});
-    
+    setSelectedDay({ ...dayData, details: filteredData });
   };
 
   const handleCloseDetailedView = () => {
@@ -48,25 +46,23 @@ function App() {
   const groupedData = data.list.reduce((acc, item) => {
     const date = new Date(item.dt * 1000).toLocaleDateString();
     if (!acc[date]) {
-      acc[date] = { count:0, totalPop: 0, temp_min: item.main.temp_min, temp_max: item.main.temp_max, icon: item.weather[0].icon};
+      acc[date] = { count: 0, totalPop: 0, temp_min: item.main.temp_min, temp_max: item.main.temp_max, icon: item.weather[0].icon };
     }
     acc[date].count += 1;
     acc[date].totalPop += item.pop;
-    if (acc[date].temp_min > item.main.temp_min){
-      acc[date].temp_min = item.main.temp_min
+    if (acc[date].temp_min > item.main.temp_min) {
+      acc[date].temp_min = item.main.temp_min;
     }
-    if (acc[date].temp_max < item.main.temp_max){
-      acc[date].temp_max = item.main.temp_max
+    if (acc[date].temp_max < item.main.temp_max) {
+      acc[date].temp_max = item.main.temp_max;
     }
-    return acc
+    return acc;
   }, {});
 
- const dates = Object.keys(groupedData).map(date => {
-  const averagePop = (groupedData[date].totalPop / groupedData[date].count) * 100;
-  return { date, averagePop: averagePop.toFixed(1), temp_min: groupedData[date].temp_min.toFixed(0), temp_max: groupedData[date].temp_max.toFixed(0), icon:groupedData[date].icon};
- })
-
-  
+  const dates = Object.keys(groupedData).map(date => {
+    const averagePop = (groupedData[date].totalPop / groupedData[date].count) * 100;
+    return { date, averagePop: averagePop.toFixed(1), temp_min: groupedData[date].temp_min.toFixed(0), temp_max: groupedData[date].temp_max.toFixed(0), icon: groupedData[date].icon };
+  });
 
   return (
     <div className="App">
@@ -78,32 +74,32 @@ function App() {
           <option value="New York">New York</option>
           <option value="Los Angeles">Los Angeles</option>
         </select>
-
       </div>
 
       <div className='dayContainer'>
-      {dates.map((item, index) => (
-        <div className='daybox' onClick={() => handleDayClick(item)}>
-        <h5>{item.date}</h5>
-        <div className='tempContainer'>
-        <div >
-
-        <h5 className='tempBox'>highest temp!</h5>
-        <p>{item.temp_max}°F</p>
-        </div>
-        <div >
-        <h5 className='tempBox'>Lowest temp!</h5>
-        <p>{item.temp_min}°F</p>
-        </div>
-        </div>
-        <p>Rain Probability: {item.averagePop}%</p>
-
-        </div>
-      ))}
-       <div>
-         {selectedDay && <DetailedView data={selectedDay} onClose={handleCloseDetailedView} />}
+        {dates.map((item, index) => (
+          <div className='dayBox' key={index} onClick={() => handleDayClick(item)}>
+            <h5>{item.date}</h5>
+            <div className='tempContainer'>
+              <div>
+                <h5 className='tempBox'>Highest temp!</h5>
+                <p>{item.temp_max}°F</p>
+              </div>
+              <div>
+                <h5 className='tempBox'>Lowest temp!</h5>
+                <p>{item.temp_min}°F</p>
+              </div>
+            </div>
+            <p>Rain Probability: {item.averagePop}%</p>
+          </div>
+        ))}
       </div>
-      </div>
+
+      {selectedDay && (
+        <div className='detailedViewContainer'>
+          <DetailedView data={selectedDay} onClose={handleCloseDetailedView} />
+        </div>
+      )}
     </div>
   );
 }
